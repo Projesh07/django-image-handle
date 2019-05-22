07af8bae23302a11b9919b38b668c86fe8f490a8
Projesh Bhoumik <projesh.bhoumik07@gmail.com>
2019-05-22 18:34:22 +0200

    fixied some bugs

--- image.py
+++ image.py
@@ -20,47 +20,47 @@ class ImageFile(File):
             self._dimensions_cache = get_image_dimensions(self, close=close)
         return self._dimensions_cache
 
+class ImageManipulation(File):
+        def get_image_dimensions(file_or_path, close=False):
+            """
+            Returns the (width, height) of an image, given an open file or a path.  Set
+            'close' to True to close the file at the end if it is initially in an open
+            state.
+            """
+            from PIL import ImageFile as PillowImageFile
 
-def get_image_dimensions(file_or_path, close=False):
-    """
-    Returns the (width, height) of an image, given an open file or a path.  Set
-    'close' to True to close the file at the end if it is initially in an open
-    state.
-    """
-    from PIL import ImageFile as PillowImageFile
-
-    p = PillowImageFile.Parser()
-    if hasattr(file_or_path, 'read'):
-        file = file_or_path
-        file_pos = file.tell()
-        file.seek(0)
-    else:
-        file = open(file_or_path, 'rb')
-        close = True
-    try:
-        # Most of the time Pillow only needs a small chunk to parse the image
-        # and get the dimensions, but with some TIFF files Pillow needs to
-        # parse the whole file.
-        chunk_size = 1024
-        while 1:
-            data = file.read(chunk_size)
-            if not data:
-                break
+            p = PillowImageFile.Parser()
+            if hasattr(file_or_path, 'read'):
+                file = file_or_path
+                file_pos = file.tell()
+                file.seek(0)
+            else:
+                file = open(file_or_path, 'rb')
+                close = True
             try:
-                p.feed(data)
-            except zlib.error as e:
-                # ignore zlib complaining on truncated stream, just feed more
-                # data to parser (ticket #19457).
-                if e.args[0].startswith("Error -5"):
-                    pass
+                # Most of the time Pillow only needs a small chunk to parse the image
+                # and get the dimensions, but with some TIFF files Pillow needs to
+                # parse the whole file.
+                chunk_size = 1024
+                while 1:
+                    data = file.read(chunk_size)
+                    if not data:
+                        break
+                    try:
+                        p.feed(data)
+                    except zlib.error as e:
+                        # ignore zlib complaining on truncated stream, just feed more
+                        # data to parser (ticket #19457).
+                        if e.args[0].startswith("Error -5"):
+                            pass
+                        else:
+                            raise
+                    if p.image:
+                        return p.image.size
+                    chunk_size *= 2
+                return None
+            finally:
+                if close:
+                    file.close()
                 else:
-                    raise
-            if p.image:
-                return p.image.size
-            chunk_size *= 2
-        return None
-    finally:
-        if close:
-            file.close()
-        else:
-file.seek(file_pos)
+        file.seek(file_pos)
